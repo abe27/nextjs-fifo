683d006c3e64464130250bec359be32b7199fc46
Taweechai Yuenyang <krumii.it@gmail.com>
2023-01-14 13:51:00 +0700

    update new package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    fontLoaders: [
+      { loader: "@next/font/google", options: { subsets: ["latin"] } },
+    ],
+  },
   env: {
     APP_NAME: `${process.env.APP_NAME}`,
     URL_HOST: `${process.env.API_HOST}`,
